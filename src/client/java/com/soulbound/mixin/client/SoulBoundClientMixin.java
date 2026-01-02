9c9573ba8f145a5cfad4dbbe3959147fbb79ba11
alextoddslick <78877316+alextoddslick@users.noreply.github.com>
2026-01-02 12:36:11 -0700

    Renaming

--- src/client/java/com/soulbound/mixin/client/SoulBoundClientMixin.java
+++ src/client/java/com/soulbound/mixin/client/SoulBoundClientMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin.client;
+package com.soulbound.mixin.client;
 
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
@@ -7,9 +7,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
-public class ExampleClientMixin {
-	@Inject(at = @At("HEAD"), method = "run")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of Minecraft.run()V
-	}
+public class SoulBoundClientMixin {
+    @Inject(at = @At("HEAD"), method = "run")
+    private void init(CallbackInfo info) {
+        // This code is injected into the start of Minecraft.run()V
+    }
 }
